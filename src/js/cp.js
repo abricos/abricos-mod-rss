6d4fd1c2ed967bf2149fd0bbc4cd6cc7ae66e015
Alexander Kuzmin <roosit@abricos.org>
2014-01-21 23:11:15 +0400

    is admin bug fix

--- src/js/cp.js
+++ src/js/cp.js
@@ -1,27 +1,30 @@
 /**
-* @version $Id$
-* @package Abricos
-* @copyright Copyright (C) 2008 Abricos. All rights reservedd.
-* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
-*/
+ * @version $Id$
+ * @package Abricos
+ * @copyright Copyright (C) 2008 Abricos. All rights reservedd.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ */
 
 var Component = new Brick.Component();
 Component.requires = {
-	mod:[
-	     {name: 'user', files: ['cpanel.js']}
-	]
+    mod: [
+        {name: 'user', files: ['cpanel.js']}
+    ]
 };
-Component.entryPoint = function(){
-	
-	if (!Brick.env.user.isAdmin()){ return; }
-	
-	var cp = Brick.mod.user.cp;
-	var menuItem = new cp.MenuItem(this.moduleName);
-	menuItem.icon = '/modules/rss/images/rss-icon-16px.gif';
-	menuItem.titleId = 'mod.rss.cp.title';
-	menuItem.entryComponent = 'api';
-	menuItem.entryPoint = 'Brick.mod.rss.API.showRSSManagerWidget';
-	
-	cp.MenuManager.add(menuItem);
-	
+Component.entryPoint = function() {
+
+    if (Brick.Permission.check('user', '50') !== 1) {
+        return;
+    }
+
+
+    var cp = Brick.mod.user.cp;
+    var menuItem = new cp.MenuItem(this.moduleName);
+    menuItem.icon = '/modules/rss/images/rss-icon-16px.gif';
+    menuItem.titleId = 'mod.rss.cp.title';
+    menuItem.entryComponent = 'api';
+    menuItem.entryPoint = 'Brick.mod.rss.API.showRSSManagerWidget';
+
+    cp.MenuManager.add(menuItem);
+
 };
